9fee4acf4a9a7cc205010572d776316ed6520323
Dang Lu <dang.lu@helix.com>
2019-06-28 21:22:18 -0400

    closure.js updated

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,24 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const name = "Dang"
 
+function hungry(food){
+	return `${name} is hungry for ${food}`
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+
+	let count = 0;
+	return function(){
+		count = count + 1;
+		return count;
+	} 
 };
+  // Return a function that when invoked increments and returns a counter variable.
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -18,4 +28,15 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  "increment": function(){
+  	count = count +1;
+  	return count;
+  },
+  "decrement": function(){
+  	count = count -1;
+  	return count;
+  }
+
+
+
 };
